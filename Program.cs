28d65daf6309c24ed2d77f7d7cb548bb95c16e95
Vera Kippel <veki2400@student.miun.se>
2025-09-27 18:18:03 +0200

    Skapar utskrift i programmet

--- Program.cs
+++ Program.cs
@@ -1,5 +1,34 @@
-﻿
+﻿//Labb 3 Vera Kippel
+//Gästbok som visar inlägg från användare, det går att lägga till och radera inläggen.
 
-namespace guestbook{
+using System;
+using System.Runtime.CompilerServices;
 
+namespace guestbook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guestbook guestbook = new Guestbook();
+            int i = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.CursorVisible = false;
+                Console.WriteLine("G Ä S T B O K\n");
+
+                Console.WriteLine("1. Lägg till ett inlägg");
+                Console.WriteLine("2. Ta bort ett inlägg\n");
+                Console.WriteLine("X. Avsluta\n");
+
+                i = 0;
+                foreach (Post post in guestbook.getPosts())
+                {
+                    Console.WriteLine("[" + i++ + "]" + "Gäst: " + post.Guest + "Text: " + post.Message);
+                }
+            }
+        }
+    }
 }
