5a01e39f70cf13d78bf4dadf3432d278dfd3d605
Vera Kippel <veki2400@student.miun.se>
2025-09-27 18:08:45 +0200

    first commit

---
+++ Guestbook.cs
@@ -0,0 +1,50 @@
+
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace guestbook
+{
+    public class Guestbook
+    {
+        private string filename = @"guestbook.json";
+        private List<Post> posts = new List<Post>();
+
+        public Guestbook()
+        {
+            //Läs allt i filename
+            if (File.Exists(filename) == true)
+            {
+                string jsonString = File.ReadAllText(filename);
+                posts = JsonSerializer.Deserialize<List<Post>>(jsonString)!;
+            }
+        }
+        public Post addPost(string g, string t)
+        {
+            Post obj = new Post();
+            obj.Guest = g;
+            obj.Message = t;
+            save();
+            return obj;
+        }
+        public int deletePost(int index)
+        {
+            posts.RemoveAt(index);
+            save();
+            return index;
+        }
+        public List<Post> getPosts()
+        {
+            return posts;
+        }
+
+
+    //Spara alla objekt till json i filen
+        private void save()
+        {
+            string jsonString = JsonSerializer.Serialize(posts);
+            File.WriteAllText(filename, jsonString);
+        }
+
+    }
+}
---
+++ Post.cs
@@ -0,0 +1,14 @@
+namespace guestbook
+{
+    public class Post
+    {
+        public string? Guest
+        {
+            get; set;
+        }
+        public string? Message
+        {
+            get; set;
+        }
+    }
+}
